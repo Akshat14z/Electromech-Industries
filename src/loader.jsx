e7feb6b460153ab5d6e2983b8ec8c87c3944dbf9
Akshat14z <patelakshat1411@gmail.com>
2025-12-03 11:26:24 +0530

    gear

--- src/loader.jsx
+++ src/loader.jsx
@@ -3,7 +3,7 @@ import "./loader.css";
 function Loader() {
   return (
     <div className="loader-container">
-      <img src="/public/gear.png" alt="loading" className="gear-loader" />
+      <img src="gear1.png" alt="loading" className="gear-loader" />
     </div>
   );
 }
